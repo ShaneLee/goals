65339a73b2c833be267ec2a7254117951f0fcee1
Shane Lee <me@shanel.ee>
2020-06-28 14:47:56 +0100

    Update database script to have foreign key between tags and goals

--- sql/database.sql
+++ sql/database.sql
@@ -22,15 +22,16 @@ CREATE TABLE `simple_goals`.`goals` (
 
 CREATE TABLE `simple_goals`.`tags` (
     `tag_id` INT NOT NULL AUTO_INCREMENT,
+    `goal_id` INT NOT NULL,
       `tag` VARCHAR(100) NULL,
         PRIMARY KEY (`tag_id`));
 
-ALTER TABLE `simple_goals`.`goals` 
-ADD INDEX `tag_id_idx` (`tag_id` ASC);
-ALTER TABLE `simple_goals`.`goals` 
-ADD CONSTRAINT `tag_id`
-  FOREIGN KEY (`tag_id`)
-    REFERENCES `simple_goals`.`tags` (`tag_id`)
+ALTER TABLE `simple_goals`.`tags` 
+ADD INDEX `goal_id_idx` (`goal_id` ASC);
+ALTER TABLE `simple_goals`.`tags` 
+ADD CONSTRAINT `goal_id`
+  FOREIGN KEY (`goal_id`)
+    REFERENCES `simple_goals`.`goals` (`goal_id`)
       ON DELETE CASCADE
         ON UPDATE CASCADE;
         
