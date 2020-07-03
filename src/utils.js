8cabacb5dc9894ee2fe7c86354ab0ee50a37d157
Shane Lee <shaneleephoto@gmail.com>
2020-07-03 20:33:22 +0100

    Feature/format dates (#12)
    * Format dates correctly
    
    Co-authored-by: Shane Lee <me@shanel.ee>

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,20 @@
+const moment = require('moment')
+
 const utils = module.exports = {}
+
+const dateFormat = (date) => date ? moment(date).format('ddd, MMM, d, YYYY') : '' 
 
 utils.tagsToArray = (tags) => {
   return !tags ? [] : tags.trim().split(', ').filter(tag => tag).map(tag => tag.replace(',', '')) 
 }
+
+utils.dateFormatter = (rows) => {
+  return rows.map(row => {
+    row.due_date = dateFormat(row.due_date) 
+    row.time_submitted = dateFormat(row.time_submitted) 
+    row.time_completed = dateFormat(row.time_completed) 
+    return row
+    })
+}
+
 
