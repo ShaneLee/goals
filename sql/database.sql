b24d6c59589cf762b88754fa93b0adcd0453bc7d
Shane Lee <me@shanel.ee>
2020-07-03 19:44:00 +0100

    Update to have tags as a comma separated string the db

--- sql/database.sql
+++ sql/database.sql
@@ -16,22 +16,7 @@ CREATE TABLE `simple_goals`.`goals` (
   `due_date` TIMESTAMP NULL,
   `complete` TINYINT NULL DEFAULT 0,
   `time_completed` TIMESTAMP NULL,
-  `tag_id` INT NULL,
+  `tags` VARCHAR(150) NULL,
   PRIMARY KEY (`goal_id`));
 
 
-CREATE TABLE `simple_goals`.`tags` (
-    `tag_id` INT NOT NULL AUTO_INCREMENT,
-    `goal_id` INT NOT NULL,
-    `tag` VARCHAR(100) NULL,
-    PRIMARY KEY (`tag_id`));
-
-ALTER TABLE `simple_goals`.`tags` 
-ADD INDEX `goal_id_idx` (`goal_id` ASC);
-ALTER TABLE `simple_goals`.`tags` 
-ADD CONSTRAINT `goal_id`
-  FOREIGN KEY (`goal_id`)
-    REFERENCES `simple_goals`.`goals` (`goal_id`)
-      ON DELETE CASCADE
-        ON UPDATE CASCADE;
-        
